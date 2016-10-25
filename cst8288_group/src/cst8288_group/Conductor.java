65bf809b478c908b24d790ec10ec0d2b6a8fe951
null <nullaffinity@gmail.com>
2016-10-25 18:03:12 -0400

    Conductor class
    We don't need a conductor class, unless we plan to elaborate it more.

--- cst8288_group/src/cst8288_group/Conductor.java
+++ cst8288_group/src/cst8288_group/Conductor.java
@@ -1,5 +1,48 @@
 package cst8288_group;
 
-public class Conductor {
+/**
+ * Conductor extends the Person class, adding a conductor identification number.
+ * @author    Chase Thorne
+ * @version   1.0.0 Date
+ */
+public class Conductor extends Person {
 
-}
+	/* CONSTRUCTORS	--------------------------------------------------	*/
+	public Conductor(String givenName, String middleName, String familyName, Address homeAddress) {
+		super(givenName, middleName, familyName, homeAddress);
+		setConductorID(conductorID);
+	}
+
+
+	/* ACCESSORS	-----------------------------------------------------	*/
+	public int getConductorID() {
+		return this.conductorID;
+	}
+
+
+	/* MODIFIERS	-----------------------------------------------------	*/
+	public void setConductorID(int conductorID) {
+		this.conductorID = conductorID;
+	}
+
+
+	/*	NORMAL BEHAVIOR -------------------------------------------------	*/
+	/* TODO:	Place public behavior here.											*/
+
+
+
+	/* HELPER METHODS	--------------------------------------------------	*/
+	/* TODO:	Place helper methods here.												*/
+
+
+
+	/*	ENTRY POINT for STAND-ALONE OPERATION ---------------------------	*/
+
+
+
+	/* ATTRIBUTES	-----------------------------------------------------	*/
+	int conductorID;
+
+
+
+}	/*	End of CLASS:	Conductor.java			*/
