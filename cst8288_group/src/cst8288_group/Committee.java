3ef7f7b44e0933a0084f59ecc304ec9690c286b6
AdamShalla <shal0037@algonquinlive.com>
2016-10-26 15:47:36 -0400

    Added Venue, Committee classes

---
+++ cst8288_group/src/cst8288_group/Committee.java
@@ -0,0 +1,138 @@
+package cst8288_group;
+
+/**
+ * Committee Class
+ * This class represents the Symphony Management Committee.  
+ * It includes 4 people: the chairman, vice-chairman, secretary, and treasurer.
+ * Author    Adam Shalla
+ * @version   1.0.0	2016-10-26
+ */
+
+public class Committee	{
+
+	/* CONSTRUCTORS	--------------------------------------------------	*/
+	public Committee()
+	{
+		
+	}
+
+	/**
+	 * Constructor for Committee class to set the committee officers
+	 * @param chairman		A person representing the chairman
+	 * @param viceChairman	A person representing the vice-chairman
+	 * @param secretary		A person representing the secretary
+	 * @param treasurer		A person representing the treasurer
+	 */
+	public Committee(Person chairman, Person viceChairman, Person secretary, Person treasurer)
+	{
+		this.chairman = chairman;
+		this.viceChairman = viceChairman;
+		this.secretary = secretary;
+		this.treasurer = treasurer;
+	}
+
+	/* ACCESSORS	-----------------------------------------------------	*/
+	/**
+	 * @return Person The committee chairman
+	 */
+	public Person getChairman()
+	{
+		return chairman;
+	}
+
+	/**
+	 * @return Person The committee vice-chairman
+	 */
+	public Person getViceChairman()
+	{
+		return viceChairman;
+	}
+
+	/**
+	 * @return Person The committee secretary
+	 */
+	public Person getSecretary()
+	{
+		return secretary;
+	}
+
+	/**
+	 * @return Person The committee treasurer
+	 */
+	public Person getTreasurer()
+	{
+		return treasurer;
+	}
+
+
+	/* MODIFIERS	-----------------------------------------------------	*/
+	/**
+	 * @param chairman The committee chairman
+	 */
+	public void setChairman(Person chairman)
+	{
+		this.chairman = chairman;
+	}
+
+	/**
+	 * @param viceChairman The committee vice-chairman
+	 */
+	public void setViceChairman(Person viceChairman)
+	{
+		this.viceChairman = viceChairman;
+	}
+
+	/**
+	 * @param secretary The committee secretary
+	 */
+	public void setSecretary(Person secretary)
+	{
+		this.secretary = secretary;
+	}
+
+	/**
+	 * @param treasurer The committee treasurer
+	 */
+	public void setTreasurer(Person treasurer)
+	{
+		this.treasurer = treasurer;
+	}
+
+
+	/*	NORMAL BEHAVIOR -------------------------------------------------	*/
+	/**
+	 * @return The four committee officers, and any other members
+	 */
+	public String toString()
+	{
+		return  "Chairman: " + this.chairman + 
+				" Vice-Chairman: " + this.viceChairman + 
+				" Secretary: " + this.secretary + 
+				" Treasurer: " + this.treasurer;
+	}
+
+
+	/* HELPER METHODS	--------------------------------------------------	*/
+	/* TODO:	Place helper methods here.												*/
+
+
+	/* ATTRIBUTES	-----------------------------------------------------	*/
+	/**
+	 * The chairman of the committee
+	 */
+	private Person chairman;
+	/**
+	 * The vice-chairman of the committee
+	 */
+	private Person viceChairman;
+	/**
+	 * The secretary of the committee
+	 */
+	private Person secretary;
+	/**
+	 * The treasurer of the committee
+	 */
+	private Person treasurer;
+
+
+}	/*	End of CLASS:	Committee.java			*/
---
+++ cst8288_group/src/cst8288_group/Venue.java
@@ -0,0 +1,115 @@
+package cst8288_group;
+
+/**
+ * Venue Class
+ * This class represents the concert Venue in the Symphony Management System.
+ * It includes an address, the name of the venue, and the maximum capacity for the venue.
+ * Author    Adam Shalla
+ * @version   1.0.0	2016-10-26
+ */
+
+public class Venue	{
+
+	/* CONSTRUCTORS	--------------------------------------------------	*/
+	public Venue()
+	{
+		
+	}
+
+	/**
+	 * Constructor for Committee class to set the name, location, and max capacity of the venue
+	 * @param location		An address represening the location of the venue
+	 * @param locationName	A string representing the name of the venue
+	 * @param maxCapacity	An int representing the maximum capacity of the venue
+	 */
+	public Venue(Address location, String locationName, int maxCapacity)
+	{
+		this.location = location;
+		this.locationName = locationName;
+		this.maxCapacity = maxCapacity;
+	}
+
+	/* ACCESSORS	-----------------------------------------------------	*/
+	/**
+	 * @return Address of the venue
+	 */
+	public Address getLocation()
+	{
+		return location;
+	}
+
+	/**
+	 * @return String containing the venue name
+	 */
+	public String getLocationName()
+	{
+		return locationName;
+	}
+	
+	/**
+	 * @return int representing the maximum capacity of the venue
+	 */
+	public int getMaxCapacity()
+	{
+		return maxCapacity;
+	}
+
+	/* MODIFIERS	-----------------------------------------------------	*/
+	/**
+	 * @param location The address of the venue
+	 */
+	public void setLocation(Address location)
+	{
+		this.location = location;
+	}
+	
+	/**
+	 * @param locationName The name of the venue
+	 */
+	public void setLocationName(String locationName)
+	{
+		this.locationName = locationName;
+	}
+	
+	/**
+	 * @param maxCapacity The maximum capacity of the venue
+	 */
+	public void setMaxCapacity(int maxCapacity)
+	{
+		this.maxCapacity = maxCapacityn;
+	}
+
+
+	/*	NORMAL BEHAVIOR -------------------------------------------------	*/
+	/**
+	 * @return The venue name, location, and max capacity variables respectively
+	 */
+	public String toString()
+	{
+		return  "Venue: " + this.getLocationName() + 
+				" Address: " + this.getLocation() + 
+				" Max capacity: " + this.getMaxCapacity();
+	}
+
+
+	/* HELPER METHODS	--------------------------------------------------	*/
+	/* TODO:	Place helper methods here.												*/
+
+
+	/* ATTRIBUTES	-----------------------------------------------------	*/
+	/**
+	 * The address of the venue
+	 */
+	private Address location;
+	/**
+	 * The name of the venue
+	 */
+	private String locationName;
+	/**
+	 * The maximum capacity of the venue
+	 */
+	private int maxCapacity;
+
+
+
+}	/*	End of CLASS:	Venue.java			*/
