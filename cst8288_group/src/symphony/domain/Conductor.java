3ea18f9977b7f773804d9db08dfdf1affa98360c
null <nullaffinity@gmail.com>
2016-11-23 18:24:14 -0500

    Reimplementing Conductor

---
+++ cst8288_group/src/symphony/domain/Conductor.java
@@ -0,0 +1,10 @@
+package symphony.domain;
+
+public class Conductor extends Person {
+	
+public Conductor (Name name, Address homeAddress, PhoneNumber phoneNumber, String id) {
+		
+		super(name, homeAddress, phoneNumber, id);
+	}
+
+}
