29975d37ca2456d784034a1ce33d17317240f61e
DaisyChubb <homm0004@algonquincollege.com>
2016-11-22 20:29:34 -0500

    Created Test Classes for Committee, Customer and Venue

---
+++ cst8288_group/src/symphony/test/Test_Venue.java
@@ -0,0 +1,12 @@
+package symphony.test;
+/**
+ * JUnit tests for the Venue class from the "symphony" project
+ * 
+ * @author Ash-Lee Hommy 040840815
+ * @version 1.0.0
+ * 
+ *
+ */
+public class Test_Venue {
+
+}
